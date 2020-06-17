40fb09b257a82967f05294dd59cb6e8dd786870a
M.Ahsan <ahsan.afzal@pakeventures.com>
2020-06-17 16:54:24 +0500

    Admin's access added

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'users/login'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :products
-
   
+  resources :products, path: '/admin/products'
+
   root 'products#index'
 end
